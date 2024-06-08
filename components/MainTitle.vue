1fab6316a4550e1193aaae2565211af91267d120
n.ivanov <n.ivanov@involta.ru>
2024-06-08 20:44:06 +0300

    add title coponent

---
+++ components/MainTitle.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  defineSlots<{
+    default: () => any;
+  }>();
+</script>
+
+<template>
+  <h2 class="title">
+    <slot />
+  </h2>
+</template>
+
+<style scoped lang="scss">
+  .title {
+    @apply font-bold text-[41px] leading-[120%] text-[--black];
+  }
+</style>
