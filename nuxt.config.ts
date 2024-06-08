3000041b47eb0c1190d4b1263b5931571e818924
n.ivanov <n.ivanov@involta.ru>
2024-06-08 15:14:40 +0300

    connetc css file to nuxt.config.ts and create el-button css file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  css: ["~/assets/scss/styles.scss"],
   modules: [
     "@nuxtjs/tailwindcss",
     "@element-plus/nuxt",
