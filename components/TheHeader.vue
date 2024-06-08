e947dd4c9e9cb64f9a178aa0ff611a219b7afd85
n.ivanov <n.ivanov@involta.ru>
2024-06-08 15:14:03 +0300

    add basic structure of scss header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -29,4 +29,38 @@
   </header>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  .header {
+    @apply py-[12px];
+    // .header__container
+    &__container {
+      @apply flex justify-between items-center gap-[10px];
+    }
+    // .header__menu
+    &__menu {
+    }
+    // .header__actions
+    &__actions {
+      @apply flex gap-[30px] items-center;
+    }
+    // .header__phone
+    &__phone {
+    }
+    // .header__registration
+    &__registration {
+    }
+  }
+  .menu {
+    @apply flex gap-[30px] items-center;
+    // .menu__logo
+    &__logo {
+    }
+    // .menu__list
+    &__list {
+      @apply flex gap-[30px] items-center;
+    }
+    // .menu__item
+    &__item {
+    }
+  }
+</style>
