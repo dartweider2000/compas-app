edcb6d48e3810d4924aac98824899535f52fe2e9
n.ivanov <n.ivanov@involta.ru>
2024-06-08 17:34:37 +0300

    create burger component

---
+++ components/UI/UIBurger.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+  const isOpen = defineModel<boolean>();
+</script>
+
+<template>
+  <button class="burger" :class="{ burger_active: isOpen }">
+    <span></span>
+  </button>
+</template>
+
+<style scoped lang="scss">
+  .burger {
+    @apply relative w-[30px] h-[20px];
+
+    &::before,
+    &::after {
+      content: "";
+    }
+
+    &::before,
+    &::after,
+    & > span {
+      @apply absolute w-[100%] h-[2px] bg-[--white];
+      transition: var(--trans);
+    }
+
+    &::before {
+      @apply top-0;
+    }
+
+    &::after {
+      @apply bottom-0;
+    }
+
+    & > span {
+      @apply top-[50%] translate-y-[-50%];
+    }
+
+    &_active {
+      &::before {
+        @apply top-[50%] rotate-[45deg] translate-y-[-50%];
+      }
+
+      &::after {
+        @apply bottom-[50%] rotate-[-45deg] translate-y-[-50%];
+      }
+
+      & > span {
+        @apply scale-0;
+      }
+    }
+  }
+</style>
