1726a5b58f06282351b1efe1b0e56c96ad74ad25
n.ivanov <n.ivanov@involta.ru>
2024-06-09 20:55:59 +0300

    add article layout

---
+++ layouts/article.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="article">
+    <div class="article__bread-crumbs">
+      <slot name="bread-crumbs" />
+    </div>
+    <MainTitle class="article__title">
+      <slot name="title" />
+    </MainTitle>
+    <div class="article__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .article {
+    // .article__bread-crumbs
+    &__bread-crumbs {
+    }
+    // .article__title
+    &__title {
+    }
+    // .article__content
+    &__content {
+    }
+  }
+</style>
