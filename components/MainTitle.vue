28a1c87597a5699681469fedcc4a71ada3fa75bf
n.ivanov <n.ivanov@involta.ru>
2024-06-09 09:46:43 +0300

    add media to main title

--- components/MainTitle.vue
+++ components/MainTitle.vue
@@ -13,5 +13,9 @@
 <style scoped lang="scss">
   .title {
     @apply font-bold text-[41px] leading-[120%] text-[--black];
+
+    @media (max-width: 768px) {
+      @apply text-[30px];
+    }
   }
 </style>
