4e6364e736343c55a67b04dfcfffd4ea2db26a07
n.ivanov <n.ivanov@involta.ru>
2024-06-09 10:45:35 +0300

    start do plus card block - create this component

---
+++ components/MainPage/MainPagePlusCards.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="plus-block">
+    <MainContainer class="plus-block__container">
+      <MainTitle class="plus-block__title"
+        >Плюсы использования сервиса «Компас» для анализа штрафов
+      </MainTitle>
+    </MainContainer>
+  </section>
+</template>
+
+<style scoped lang="scss"></style>
