309c1729428b1a0f4d038107367dad43198c885a
n.ivanov <n.ivanov@involta.ru>
2024-06-09 21:59:48 +0300

    add fishing pages

---
+++ pages/contacts.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import SvgBreadCrumbsIcon from "~/components/Svg/SvgBreadCrumbsIcon.vue";
+</script>
+
+<template>
+  <NuxtLayout name="article">
+    <template #bread-crumbs>
+      <ElBreadcrumb class="bread-crumb" :separator-icon="SvgBreadCrumbsIcon">
+        <ElBreadcrumbItem to="/">Главная</ElBreadcrumbItem>
+        <ElBreadcrumbItem>Контакты</ElBreadcrumbItem>
+      </ElBreadcrumb>
+    </template>
+    <template #title>Контакты</template>
+  </NuxtLayout>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ pages/documents/info.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import SvgBreadCrumbsIcon from "~/components/Svg/SvgBreadCrumbsIcon.vue";
+</script>
+
+<template>
+  <NuxtLayout name="article">
+    <template #bread-crumbs>
+      <ElBreadcrumb class="bread-crumb" :separator-icon="SvgBreadCrumbsIcon">
+        <ElBreadcrumbItem to="/">Главная</ElBreadcrumbItem>
+        <ElBreadcrumbItem to="/">Документы</ElBreadcrumbItem>
+        <ElBreadcrumbItem>Инвормация</ElBreadcrumbItem>
+      </ElBreadcrumb>
+    </template>
+    <template #title>Инвормация</template>
+  </NuxtLayout>
+</template>
+
+<style scoped lang="scss"></style>
