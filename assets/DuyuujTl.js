0a595104716b877588f47e029f4bf4e01c78f0ce
n.ivanov <n.ivanov@involta.ru>
2024-06-09 23:06:41 +0300

    Updates

---
+++ assets/DuyuujTl.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CTVvlR4y.js";import{y as t,J as n,_}from"./i-imgPZh.js";const c={},r={class:"container"};function s(e,a){return t(),n("div",r,[_(e.$slots,"default",{},void 0,!0)])}const i=o(c,[["render",s],["__scopeId","data-v-4e62c7de"]]);export{i as _};
