2a7b1486bd8c97f59a4a1d693313d52b5c5ea7b5
n.ivanov <n.ivanov@involta.ru>
2024-06-09 19:31:30 +0300

    create popup component

---
+++ components/MainPage/MianPagePopup.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="popup">
+    <div class="popup__body">
+      <div class="popup__content">
+        <UIBurger :model-value="false" />
+        <div class="popup__video">
+          <iframe
+            src="https://www.youtube.com/embed/xKVcVSYmesU?si=Zoq0GSDbbszPr8Y0?autoplay=1"
+            title="YouTube video player"
+            loading="lazy"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
